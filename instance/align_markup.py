b0c3468500f785b6f4232f8bb1e33e9f1791b49b
Pavel Komov <kmvpvl@ya.ru>
2020-10-01 10:06:32 +0400

    example2 factory simple

---
+++ instance/align_markup.py
@@ -0,0 +1,22 @@
+def joinAll(l, t, r, b):
+	return ";".join([",".join(["%.14f" % l, "%.14f" % t]), ",".join(["%.14f" % r, "%.14f" % b])])
+	
+s = "53.38126066537966,50.17000911153846;53.38055422432430,50.17063713846153"
+ltrb = s.split(";")
+l = float(ltrb[0].split(",")[0])
+t = float(ltrb[0].split(",")[1])
+r = float(ltrb[1].split(",")[0])
+b = float(ltrb[1].split(",")[1])
+
+w = r - l
+h = b - t
+
+# to right
+l += 1.5 * w
+r += 1.5 * w
+print (joinAll(l, t, r, b))
+
+# down
+t += 1.5 * h
+b += 1.5 * h
+print (joinAll(l, t, r, b))
